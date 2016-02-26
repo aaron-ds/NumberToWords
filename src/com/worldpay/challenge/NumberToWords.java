dbe8ee1e016bff27d2ae9eb8118a9b2a91c3ed91
aaron-ds <adesouza@iee.org>
2016-02-26 10:09:58 +0000

    Update NumberToWords.java

--- src/com/worldpay/challenge/NumberToWords.java
+++ src/com/worldpay/challenge/NumberToWords.java
@@ -1,6 +1,15 @@
 package com.worldpay.challenge;
 
+/**
+ * An interface used to convert an int into a text representation. Implementations may be in any language
+ * and may impose limits on the range of ints that can be converted.
+ */
 public interface NumberToWords {
 
+    /**
+     * Converts an int to a word representation
+     * @param i number to convert
+     * @return String representing the number in words
+     */
     String toWords(int i);
 }
